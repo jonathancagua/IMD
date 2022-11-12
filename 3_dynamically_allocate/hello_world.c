ac315703e44f2434e7fda3a34d4ad8d7bd807d70
Jonathan Cagua <jonathan.cagua@gmail.com>
2022-11-12 13:41:13 -0500

    FIX: line

--- 3_dynamically_allocate/hello_world.c
+++ 3_dynamically_allocate/hello_world.c
@@ -11,15 +11,15 @@ dev_t dev = 0;
 */
 static int __init hello_world_init(void)
 {
-        /*Allocating Major number*/
-        if((alloc_chrdev_region(&dev, 0, 1, "jcagua_dev")) <0){
-                pr_info("Cannot allocate major number for device 1\n");
-                return -1;
-        }
-        pr_info("Major = %d Minor = %d \n",MAJOR(dev), MINOR(dev));
-        pr_info("Kernel Module Inserted Successfully...\n");
-        
-        return 0;
+    /*Allocating Major number*/
+    if((alloc_chrdev_region(&dev, 0, 1, "jcagua_dev")) <0){
+            pr_info("Cannot allocate major number for device 1\n");
+            return -1;
+    }
+    pr_info("Major = %d Minor = %d \n",MAJOR(dev), MINOR(dev));
+    pr_info("Kernel Module Inserted Successfully...\n");
+    
+    return 0;
 }
 
 /*
@@ -27,8 +27,8 @@ static int __init hello_world_init(void)
 */
 static void __exit hello_world_exit(void)
 {
-        unregister_chrdev_region(dev, 1);
-        pr_info("Kernel Module Removed Successfully...\n");
+    unregister_chrdev_region(dev, 1);
+    pr_info("Kernel Module Removed Successfully...\n");
 }
  
 module_init(hello_world_init);
