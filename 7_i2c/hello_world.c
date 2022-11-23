00411a4e44c4416f914808560250ff9386fce60e
Jonathan Cagua <jonathan.cagua@gmail.com>
2022-11-22 20:31:28 -0500

    FIX: iic module

--- 7_i2c/hello_world.c
+++ 7_i2c/hello_world.c
@@ -4,6 +4,12 @@
 #include <linux/i2c.h>
 #include <linux/delay.h>
 #include <linux/kernel.h>
+#include <linux/kdev_t.h>
+#include <linux/fs.h>
+#include <linux/err.h>
+#include <linux/cdev.h>
+#include <linux/device.h>
+
  
 #define I2C_BUS_AVAILABLE   (          1 )              // I2C Bus available in our Raspberry Pi
 #define SLAVE_DEVICE_NAME   ( "ETX_SENS" )              // Device and Driver Name
@@ -12,6 +18,70 @@
 static struct i2c_adapter *etx_i2c_adapter     = NULL;  // I2C Adapter Structure
 static struct i2c_client  *etx_i2c_client_cap_sense = NULL;  // I2C Cient Structure (In our case it is CAP1188)
  
+dev_t dev = 0;
+static struct class *dev_class;
+static struct cdev etx_cdev;
+
+/*************** Driver functions **********************/
+static int etx_open(struct inode *inode, struct file *file);
+static int etx_release(struct inode *inode, struct file *file);
+static ssize_t etx_read(struct file *filp, 
+                char __user *buf, size_t len,loff_t * off);
+static ssize_t etx_write(struct file *filp, 
+                const char *buf, size_t len, loff_t * off);
+/******************************************************/
+
+//File operation structure 
+static struct file_operations fops =
+{
+  .owner          = THIS_MODULE,
+  .read           = etx_read,
+  .write          = etx_write,
+  .open           = etx_open,
+  .release        = etx_release,
+};
+/********************************************************/
+
+static int etx_open(struct inode *inode, struct file *file)
+{
+    /*
+    ** This function will be called when we open the Device file
+    */ 
+    pr_info("Device File Opened...!!!\n");
+    return 0;
+}
+
+
+static int etx_release(struct inode *inode, struct file *file)
+{
+    /*
+    ** This function will be called when we close the Device file
+    */
+    pr_info("Device File Closed...!!!\n");
+    return 0;
+}
+
+static ssize_t etx_read(struct file *filp, 
+                char __user *buf, size_t len, loff_t *off)
+{
+    /*
+    ** This function will be called when we read the Device file
+    */ 
+    pr_info("Driver Read Function Called...!!!\n");
+    return 0;
+}
+
+static ssize_t etx_write(struct file *filp, 
+                const char __user *buf, size_t len, loff_t *off)
+{
+    /*
+    ** This function will be called when we write the Device file
+    */ 
+    pr_info("Writting device file...!!!\n");
+    return 0;
+}
+
+/*******************************************************/
 static int I2C_write(unsigned char *buf, unsigned int len)
 {
     /*
@@ -124,12 +194,43 @@ static void __exit etx_driver_exit(void)
 {
     i2c_unregister_device(etx_i2c_client_cap_sense);
     i2c_del_driver(&etx_cap_driver);
+    device_destroy(dev_class,dev);
+    class_destroy(dev_class);
+    cdev_del(&etx_cdev);
+    unregister_chrdev_region(dev, 1);
     pr_info("Driver Removed!!!\n");
 }
 
 static int __init etx_driver_init(void)
 {
     int ret = -1;
+    /*Allocating Major number*/
+    if((alloc_chrdev_region(&dev, 0, 1, "etx_Dev")) <0){
+        pr_err("Cannot allocate major number\n");
+        return -1;
+    }
+    pr_info("Major = %d Minor = %d \n",MAJOR(dev), MINOR(dev));
+
+    /*Creating cdev structure*/
+    cdev_init(&etx_cdev,&fops);
+
+    /*Adding character device to the system*/
+    if((cdev_add(&etx_cdev,dev,1)) < 0){
+        pr_err("Cannot add the device to the system\n");
+        return -1;
+    }
+
+    /*Creating struct class*/
+    if((dev_class = class_create(THIS_MODULE,"etx_class")) == NULL){
+        pr_err("Cannot create the struct class\n");
+        return -1;
+    }
+
+    /*Creating device*/
+    if((device_create(dev_class,NULL,dev,NULL,"etx_device")) == NULL){
+        pr_err( "Cannot create the Device \n");
+        return -1;
+    }
     etx_i2c_adapter     = i2c_get_adapter(I2C_BUS_AVAILABLE);
     
     if( etx_i2c_adapter != NULL )
